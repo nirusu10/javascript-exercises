63cebebc5c0daeb538c330335deb20b87235ac92
Nils Matic <matic1909@gmail.com>
2024-06-27 16:51:00 +0200

    added solution for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...args) {
+  const resultArray = []
+  array.forEach((item) => {
+    if (!args.includes(item)) resultArray.push(item)
+  })
+  return resultArray
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
