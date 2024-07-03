fbd5e73b66965a7e9f0442924b96341e5bee0240
Nils Matic <matic1909@gmail.com>
2024-07-03 12:50:44 +0200

    added solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num !== 'number') {
+    num = parseInt(num)
+  }
+  if (num < 0) return 'OOPS'
+  if (num === 0) return 0
 
-};
+  let prev = 1
+  let prevPrev = 0
+
+  for (let i = 2; i <= num; i++) {
+    let current = prevPrev + prev
+    prevPrev = prev
+    prev = current
+  }
+
+  return prev
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
