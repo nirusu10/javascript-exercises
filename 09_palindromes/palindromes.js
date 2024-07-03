47c4480b3988374bc391a5dc4d560e74a77a3003
Nils Matic <matic1909@gmail.com>
2024-07-03 12:45:41 +0200

    added solution for 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  let index = 0
+  const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('')
 
-};
+  while (index < cleanedString.length / 2) {
+    if (
+      cleanedString[index] !== cleanedString[cleanedString.length - index - 1]
+    )
+      return false
+    index++
+  }
+  return true
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
