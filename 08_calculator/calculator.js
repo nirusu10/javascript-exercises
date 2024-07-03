18a23d5971a85ab1fea2fdf651b328fb131a035f
Nils Matic <matic1909@gmail.com>
2024-07-03 12:37:42 +0200

    added solution for 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
-};
+const add = function (a, b) {
+  return a + b
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (a, b) {
+  return a - b
+}
 
-const sum = function() {
-	
-};
+const sum = function (numbers) {
+  let sum = 0
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+  }
+  return sum
+}
 
-const multiply = function() {
+const multiply = function (numbers) {
+  if (numbers.length < 2) {
+    return 'ERROR'
+  }
+  let product = 1
 
-};
+  for (let i = 0; i < numbers.length; i++) {
+    product = product * numbers[i]
+  }
 
-const power = function() {
-	
-};
+  return product
+}
 
-const factorial = function() {
-	
-};
+const power = function (base, exponent) {
+  return Math.pow(base, exponent)
+}
+
+const factorial = function (num) {
+  if (num === 0 || num === 1) return 1
+  let product = 1
+  for (let i = 1; i <= num; i++) {
+    product = product * i
+  }
+  return product
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
-};
+  factorial,
+}
