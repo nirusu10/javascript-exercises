70e26d1e1eb243d944b499eff1753e5a7d4f4d87
Nils Matic <matic1909@gmail.com>
2024-11-05 15:44:56 +0100

    added solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function (string) {
+  let reversedString = ''
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString = reversedString + string.charAt(i)
+  }
+  return reversedString
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
