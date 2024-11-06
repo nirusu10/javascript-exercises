5253c26ab1a6375f0fc6805cb854a459bc775b00
Nils Matic <matic1909@gmail.com>
2024-11-06 14:28:36 +0100

    added solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    )
+    return currentAge > oldestAge ? currentPerson : oldestPerson
+  })
+}
 
-};
+function getAge(birth, death) {
+  if (!death) {
+    return new Date().getFullYear() - birth
+  } else {
+    return death - birth
+  }
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
