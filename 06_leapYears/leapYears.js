6c3abdc2826b1cb4d969032c7b7f10fe03d2d975
Nils Matic <173457306+nirusu10@users.noreply.github.com>
2025-04-26 14:27:16 +0200

    add solution for 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
-};
+const leapYears = function (year) {
+  if (year % 400 === 0) return true
+  if (year % 100 === 0) return false
+  return year % 4 === 0
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
