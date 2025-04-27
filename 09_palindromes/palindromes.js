58839055179a5cddff14155060d9d33439c3b711
Nils Matic <173457306+nirusu10@users.noreply.github.com>
2025-04-27 16:42:48 +0200

    add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+  const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((c) => 'abcdefghijklmnopqrstuvwxyz0123456789'.includes(c))
+    .join('')
+  let start = 0
+  let end = cleanedString.length - 1
+  while (end > start) {
+    if (cleanedString[start] !== cleanedString[end]) return false
+    start++
+    end--
+  }
+  return true
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
