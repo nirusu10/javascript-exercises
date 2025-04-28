28ffc4ccaa7d05b3be574c74feb7c14ffe551a00
Nils Matic <173457306+nirusu10@users.noreply.github.com>
2025-04-28 13:24:38 +0200

    add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let number = parseInt(num)
 
-};
+  if (typeof number !== 'number' || number < 0) return 'OOPS'
+  if (number === 0) return 0
+
+  let prevPrev = 0
+  let prev = 1
+
+  for (let i = 2; i <= number; i++) {
+    let current = prev + prevPrev
+    prevPrev = prev
+    prev = current
+  }
+
+  return prev
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
